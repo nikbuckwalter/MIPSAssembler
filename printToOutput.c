1d31ae837f4c0c06d1b4d0d243868297cb66aec9
nikbuckwalter <nikbuckwalter@vt.edu>
2022-05-01 21:51:27 -0400

    org

--- printToOutput.c
+++ printToOutput.c
@@ -8,12 +8,10 @@
 void printRegR(char* instruction, FILE* output) {
     char* mnemonic = strtok(instruction, " ");
     fprintf(output, "%s", mnemonic);
-    char* rd = strtok(instruction, ", ");
-    fprintf(output, "%s", rd);
-    char* rs = strtok(instruction, ", ");
-    fprintf(output, "%s", rs);
-    char* rt = strtok(instruction, ", ");
-    fprintf(output, "%s", rt);
+    char* Op = getMnemOp(mnemonic);
+    fprintf(output, "%s", Op);
+
+    
     
 
 }
